a1725e911db6697578ed79aabbc4951e94cd3612
AntonovAlexander <antonov.alex.alex@gmail.com>
2017-06-18 20:46:15 +0300

    Readding lip binaries

---
+++ lip/pipe/include/ac_pipe_cmd_tcl.hpp
@@ -0,0 +1,17 @@
+/*
+ * ac_rpipe_cmd_tcl.hpp
+ *
+ *  Created on: 31.01.2017
+ *      Author: Alexander Antonov <antonov.alex.alex@gmail.com>
+ *     License: See LICENSE file for details
+ */
+
+#ifndef AC_SYNC_CMD_TCL_H_
+#define AC_SYNC_CMD_TCL_H_
+
+#include "ac_utils.hpp"
+#include "ac_pipe.hpp"
+
+int TCL_pipe_InitCmds(Tcl_Interp *inter);
+
+#endif /* AC_SYNC_CMD_TCL_H_ */
