3d0fc8409ffee33508cce789fb624b3ebede8d37
AntonovAlexander <antonov.alex.alex@gmail.com>
2017-10-12 12:58:49 +0300

    Restructuring CPU design logic

--- designs/activecore/riscv/riscv_3stage/prj.tcl
+++ designs/activecore/riscv/riscv_3stage/prj.tcl
@@ -1,12 +1,13 @@
 ## See LICENSE file for license details
 
-set MLIP_PATH /home/alexander/work/activecore/prj/mlip
+set MLIP_PATH /home/alexander/work/activecore/mlip
 load [file join $MLIP_PATH activecore/cpp/Release/libactivecore.so]
 source [file join $MLIP_PATH activecore activecore.tcl]
 
 #ActiveCore::debug_set
 ActiveCore_Reset
 
+source ../riscv_pipe.tcl
 source riscv_3stage.tcl
 
 set filename riscv_3stage.v
