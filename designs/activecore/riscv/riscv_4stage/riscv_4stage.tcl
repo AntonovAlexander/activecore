b9f3e8ecd50adf3fe79eb62d04ad59ab84fb110d
AntonovAlexander <antonov.alex.alex@gmail.com>
2017-10-13 14:43:57 +0300

    Improving timing for riscv_4stage

--- designs/activecore/riscv/riscv_4stage/riscv_4stage.tcl
+++ designs/activecore/riscv/riscv_4stage/riscv_4stage.tcl
@@ -16,28 +16,17 @@ rtl::module riscv_4stage
 			riscv_pipe::process_pc
 			pipe::mcopipe_rdreq instr_mem [cnct {curinstr_addr curinstr_addr}]
 
-		pipe::pstage EXEC
+		pipe::pstage IDECODE
 
 			pipe::mcopipe_resp instr_mem instr_code
 			riscv_pipe::process_decode
 			riscv_pipe::process_regfetch
-			
-			# pipeline WB forwarding
-			begif [s&& [pipe::issucc WB] [pipe::prr WB rd_req]]
-				begif [s== [pipe::prr WB rd_addr] rs1_addr]
-					s= rs1_rdata [pipe::prr WB rd_wdata]
-				endif
-				begif [s== [pipe::prr WB rd_addr] rs2_addr]
-					s= rs2_rdata [pipe::prr WB rd_wdata]
-				endif
-			endif
-			##
 
 			# pipeline MEM forwarding
 			begif [s&& [pipe::isworking MEM] [pipe::prr MEM rd_req]]
 				begif [s== [pipe::prr MEM rd_addr] rs1_addr]
 					begif [pipe::prr MEM rd_rdy]
-						pipe::accum rs1_rdata [pipe::prr MEM rd_wdata]
+						s= rs1_rdata [pipe::prr MEM rd_wdata]
 					endif
 					begelse
 						pipe::pstall
@@ -45,7 +34,7 @@ rtl::module riscv_4stage
 				endif
 				begif [s== [pipe::prr MEM rd_addr] rs2_addr]
 					begif [pipe::prr MEM rd_rdy]
-						pipe::accum rs2_rdata [pipe::prr MEM rd_wdata]
+						s= rs2_rdata [pipe::prr MEM rd_wdata]
 					endif
 					begelse
 						pipe::pstall
@@ -53,11 +42,32 @@ rtl::module riscv_4stage
 				endif
 			endif
 
+			# pipeline EXEC forwarding
+			begif [s&& [pipe::isworking EXEC] [pipe::prr EXEC rd_req]]
+				begif [s== [pipe::prr EXEC rd_addr] rs1_addr]
+					begif [pipe::prr EXEC rd_rdy]
+						s= rs1_rdata [pipe::prr EXEC rd_wdata]
+					endif
+					begelse
+						pipe::pstall
+					endif
+				endif
+				begif [s== [pipe::prr EXEC rd_addr] rs2_addr]
+					begif [pipe::prr EXEC rd_rdy]
+						s= rs2_rdata [pipe::prr EXEC rd_wdata]
+					endif
+					begelse
+						pipe::pstall
+					endif
+				endif
+			endif
+
+		pipe::pstage EXEC
+
 			riscv_pipe::process_alu
 			riscv_pipe::process_rd_csr_prev
 			riscv_pipe::process_jump_op
 			riscv_pipe::process_mem_reqdata
-
 
 		pipe::pstage MEM
 			
@@ -70,13 +80,6 @@ rtl::module riscv_4stage
 				endif
 				begelse
 					pipe::mcopipe_rdreq data_mem [cnct {mem_addr mem_be mem_wdata}]
-				endif
-			endif
-
-		pipe::pstage WB
-			
-			begif mem_req
-				begif [s! mem_cmd]
 					begif [pipe::mcopipe_resp data_mem mem_rdata]
 						s= rd_rdy	1
 					endif
