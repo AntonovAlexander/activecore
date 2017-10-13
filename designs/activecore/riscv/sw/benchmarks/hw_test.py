9c9e047ac3cd79387d665f74cb5d7adc658132af
Antonov <antonov.alex.alex@gmail.com>
2017-10-13 22:05:30 +0300

    Minor test update

--- designs/activecore/riscv/sw/benchmarks/hw_test.py
+++ designs/activecore/riscv/sw/benchmarks/hw_test.py
@@ -25,16 +25,24 @@ test_fail_counter = 0
 udm.cc('COM5', 921600)
 print("")
 
+def reset_buf():
+    buf_wordsize = 2048    
+    for index in range(0, buf_wordsize):
+        udm.wr(0x6000 + (index << 2), 0x0)
+
+reset_buf()
 if (hw_test_median('median.riscv') == 1):
     test_succ_counter = test_succ_counter + 1
 else:
     test_fail_counter = test_fail_counter + 1
 
+reset_buf()
 if (hw_test_qsort('qsort.riscv') == 1):
     test_succ_counter = test_succ_counter + 1
 else:
     test_fail_counter = test_fail_counter + 1
 
+reset_buf()
 if (hw_test_rsort('rsort.riscv') == 1):
     test_succ_counter = test_succ_counter + 1
 else:
