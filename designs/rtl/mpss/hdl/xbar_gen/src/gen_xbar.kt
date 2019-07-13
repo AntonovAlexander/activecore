95657820ff6dd0a2cf3de73fa811583591359b28
Alexander Antonov <antonov.alex.alex@gmail.com>
2019-07-13 13:39:27 +0300

    Adding xbar gen to mpss

---
+++ designs/rtl/mpss/hdl/xbar_gen/src/gen_xbar.kt
@@ -0,0 +1,23 @@
+import hwast.*
+import ariele.*
+
+fun main() {
+    println("xbar: generating")
+
+    val SLAVE_ADDR_WIDTH = 30
+    val SLAVE_SPACE_SIZE = 1L.shl(SLAVE_ADDR_WIDTH)
+
+    var map = addr_map()
+    for (i in 0..4) {
+        map.add(slave_entry((i * SLAVE_SPACE_SIZE).toString(), SLAVE_ADDR_WIDTH))
+    }
+
+    var xbar = ariele.xbar(("ariele_xbar"),
+        5,
+        32,
+        4,
+        hw_type(VAR_TYPE.UNSIGNED, hw_dim_static(31, 0)),
+        hw_type(VAR_TYPE.UNSIGNED, hw_dim_static(31, 0)),
+        map)
+    xbar.export_to_sv("coregen")
+}
