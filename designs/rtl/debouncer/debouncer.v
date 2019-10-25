7a9f1303057bd6f931181fa021d50a5775360e18
Alexander Antonov <antonov.alex.alex@gmail.com>
2019-10-25 21:11:20 +0300

    Debouncer update

--- designs/rtl/debouncer/debouncer.v
+++ designs/rtl/debouncer/debouncer.v
@@ -9,18 +9,17 @@
 
 module debouncer 
 #(
-	parameter FACTOR_POW = 6
+	parameter FACTOR_POW = 20
 )
 (
 	input [0:0] clk_i,
 	input [0:0] rst_i,
 	input [0:0] in,
-	output [0:0] out
+	output reg [0:0] out
 );
 
 reg in_buf0, in_buf1;
 reg [FACTOR_POW-1:0] counter;
-assign out = counter[FACTOR_POW-1];
 
 always @(posedge clk_i)
 	begin
@@ -29,18 +28,21 @@ always @(posedge clk_i)
 		in_buf0 <= 1'b0;
 		in_buf1 <= 1'b0;
 		counter <= 0;
+		out <= 1'b0;
 		end
 	else
 		begin
 		in_buf0 <= in;
 		in_buf1 <= in_buf0;
-		if (in_buf0)
+		if (in_buf1)
 			begin
 			if (counter != {FACTOR_POW{1'b1}}) counter <= counter + 1;
+			else out <= 1'b1;
 			end
 		else
 			begin
 			if (counter != 0) counter <= counter - 1;
+			else out <= 1'b0;
 			end
 		end
 	end
