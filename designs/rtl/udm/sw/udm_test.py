9091294bc321728f483fb28bcb1b64f4d76c3189
Alexander Antonov <antonov.alex.alex@gmail.com>
2020-02-13 16:32:22 +0300

    memtest update

--- designs/rtl/udm/sw/udm_test.py
+++ designs/rtl/udm/sw/udm_test.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
 from __future__ import division
 
+import random as rng
+
 import udm
 from udm import *
 
@@ -12,22 +14,34 @@ CSR_SW_ADDR     = 0x00000004
 TESTMEM_ADDR    = 0x80000000
 
 def memtest(size):
-    print("\n---- memtest started ----");
+    print("\n---- memtest started, size:", size, " ----");
     test_succ = True
+    
+    # generating test data
+    wrdata = []
     for i in range(size):
-        udm.wr(TESTMEM_ADDR + (i << 2), i)
+        wrdata.append(rng.randint(0, ((1024*1024*1024*4)-1)))
+    
+    # writing test data
+    udm.wrarr(TESTMEM_ADDR, wrdata)
+        
+    #reading test data
+    rddata = udm.rdarr32(TESTMEM_ADDR, size)
+    
+    # checking test data
     for i in range(size):
-        data_read = udm.rd(TESTMEM_ADDR + (i << 2))
-        if (data_read != i):
-            print("memtest failed on address ", hex(TESTMEM_ADDR + (i << 2)), "expected data: ", hex(i), " data read: ", hex(data_read))
+        if (rddata[i] != wrdata[i]):
+            print("memtest failed on address ", hex(TESTMEM_ADDR + (i << 2)), "expected data: ", hex(wrdata[i]), " data read: ", hex(rddata[i]))
             test_succ = False
+    
     if (test_succ):
         print("---- memtest PASSED ----\n")
     else:
         print("---- memtest FAILED ----\n")
 
+
 udm.wr(CSR_LED_ADDR, 0xaa55)
 print("Data read: ", hex(udm.rd(CSR_SW_ADDR)))
-memtest(16)
+memtest(1024)
 
 udm.discon()
