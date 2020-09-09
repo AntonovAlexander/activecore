4a0f5af7f58066918afb8b98043d2b7f036a9b43
AntonovAlexander <antonov.alex.alex@gmail.com>
2020-09-09 22:55:03 +0300

    Removing redundant files

--- designs/rtl/sigma/sw/benchmarks/common/io.h
+++
@@ -1,8 +0,0 @@
-#ifndef SIGMA_IO_H
-#define SIGMA_IO_H
-
-#define IO_LED          (*(volatile unsigned int *)(0x80000000))
-#define IO_SW           (*(volatile unsigned int *)(0x80000004))
-#define IO_MEM_ADDR   0x6000
-
-#endif // SIGMA_IO_H
--- designs/rtl/sigma/sw/benchmarks/common/isr.h
+++
@@ -1,6 +0,0 @@
-#ifndef SIGMA_ISR_H
-#define SIGMA_ISR_H
-
-int ConnectISR(int mcause, void (*new_isr)(int));
-
-#endif // SIGMA_ISR_H
