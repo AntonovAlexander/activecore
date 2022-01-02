3027066530034067b342cba6c37b7fac4c66e81d
Alexander Antonov <antonov.alex.alex@gmail.com>
2022-01-02 20:57:02 +0300

    Reverting coproc wrapper to default

--- designs/rtl/sigma_tile/hw/coproc_custom0_wrapper.sv
+++ designs/rtl/sigma_tile/hw/coproc_custom0_wrapper.sv
@@ -12,40 +12,7 @@ module coproc_custom0_wrapper (
 );
 
 assign stream_req_bus_genfifo_ack_o = stream_req_bus_genfifo_req_i;
-//assign stream_resp_bus_genfifo_req_o = 1;
-//assign stream_resp_bus_genfifo_wdata_bo = 0;
-
-logic unsigned [31:0] cur_index, max_index, max_val;
-assign stream_resp_bus_genfifo_wdata_bo = max_index;
-
-always @(posedge clk_i)
-	begin
-	if (rst_i)
-		begin
-		stream_resp_bus_genfifo_req_o <= 1'b0;
-		cur_index <= 0;
-		max_index <= 0;
-		max_val <= 0;
-		end
-	else
-		begin
-		stream_resp_bus_genfifo_req_o <= 1'b0;
-		if (stream_req_bus_genfifo_req_i)
-			begin
-			if (stream_req_bus_genfifo_rdata_bi.src0_data > max_val)
-				begin
-				max_index <= cur_index;
-				max_val <= stream_req_bus_genfifo_rdata_bi.src0_data;
-				end
-			if ((stream_req_bus_genfifo_rdata_bi.src1_data > max_val) && (stream_req_bus_genfifo_rdata_bi.src1_data > stream_req_bus_genfifo_rdata_bi.src0_data))
-				begin
-				max_index <= cur_index + 1;
-				max_val <= stream_req_bus_genfifo_rdata_bi.src1_data;
-				end
-			stream_resp_bus_genfifo_req_o <= 1'b1;
-			cur_index <= cur_index + 2;
-			end
-		end
-	end
+assign stream_resp_bus_genfifo_req_o = 1;
+assign stream_resp_bus_genfifo_wdata_bo = 0;
 
 endmodule
