a0bd5a0661067fcc425bad548bb367be00f826ad
Alexander Antonov <antonov.alex.alex@gmail.com>
2024-09-05 23:40:53 +0300

    FIFO control logic refinement

--- designs/rtl/fifo/fifo.v
+++ designs/rtl/fifo/fifo.v
@@ -1,54 +1,55 @@
-`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Create Date:    	13:11:50 05/24/2012 
 // Module Name:    	fifo 
 // Description: 	fifo buffer
-// Sourced from: 	Pong P. Chu, "FPGA Prototyping by Verilog Examples"
+// Sourced from: 	Pong P. Chu, "FPGA Prototyping by Verilog Examples" (modified)
 //////////////////////////////////////////////////////////////////////////////////
+
 module fifo
 	#(
 	parameter B=8,
 	W=4
 	)
 (
-input wire clk, reset,
-input wire rd, wr,
-input wire [B-1:0] w_data,
-output wire empty, full,
-output wire [B-1:0] r_data
+input wire clk_i, rst_i,
+input wire rd_i, wr_i,
+input wire [B-1:0] w_data_bi,
+output wire empty_o, full_o,
+output wire [B-1:0] r_data_bo
 );
 // signal declaration
 reg [B-1:0] array_reg [2**W-1:0]; // register array
 reg [W-1:0] w_ptr_reg, w_ptr_next, w_ptr_succ;
 reg [W-1:0] r_ptr_reg, r_ptr_next, r_ptr_succ;
 reg full_reg, empty_reg, full_next, empty_next;
-wire wr_en;
+wire wr_en, rd_en;
 
 // body
 // register file write operation
-always @(posedge clk)
+always @(posedge clk_i)
 	if (wr_en)
-		array_reg [w_ptr_reg] <= w_data;
+		array_reg [w_ptr_reg] <= w_data_bi;
 // register file read operation
-assign r_data = array_reg[r_ptr_reg];
+assign r_data_bo = array_reg[r_ptr_reg];
 // write enabled only when FIFO is not full
-assign wr_en = wr & ~full_reg;
+assign wr_en = wr_i & ~full_reg;
+assign rd_en = rd_i & ~empty_reg;
 //
 // fifo control logic
 // register for read and write pointers
-always @(posedge clk)
-if (reset)
+always @(posedge clk_i)
+if (rst_i)
 	begin
 	w_ptr_reg <= 0;
 	r_ptr_reg <= 0;
-	full_reg <= 1'b0;
+	full_reg  <= 1'b0;
 	empty_reg <= 1'b1;
 	end
 else
 	begin
 	w_ptr_reg <= w_ptr_next;
 	r_ptr_reg <= r_ptr_next;
-	full_reg <= full_next;
+	full_reg  <= full_next;
 	empty_reg <= empty_next;
 	end
 // next-state logic for read and write pointers
@@ -60,26 +61,27 @@ always @*
 	// default: keep old values
 	w_ptr_next = w_ptr_reg;
 	r_ptr_next = r_ptr_reg;
-	full_next = full_reg;
+	full_next  = full_reg;
 	empty_next = empty_reg;
-	case ({wr, rd})
-	// 2'b00: no op
+	case ({wr_en, rd_en})
+		// 2'b00: no op
+		
 		2'b01: // read
-		if (~empty_reg) // not empty
 			begin
 			r_ptr_next = r_ptr_succ;
 			full_next = 1'b0;
 			if (r_ptr_succ==w_ptr_reg)
 				empty_next = 1'b1;
 			end
+		
 		2'b10: // write
-		if (~full_reg) // not full
 			begin
 			w_ptr_next = w_ptr_succ ;
 			empty_next = 1'b0;
 			if (w_ptr_succ==r_ptr_reg)
 				full_next = 1'b1;
 			end
+		
 		2'b11: // write and read
 			begin
 			w_ptr_next = w_ptr_succ;
@@ -88,6 +90,6 @@ always @*
 	endcase
 end
 // output
-assign full = full_reg;
-assign empty = empty_reg;
+assign full_o = full_reg;
+assign empty_o = empty_reg;
 endmodule
